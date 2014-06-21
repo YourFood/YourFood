9ebd57decf700b52bcd51e88b57a9f6e179aac25
Yasen <ias_sv_bg@yahoo.co.uk>
2014-06-22 01:54:23 +0300

    Added click events
    For food category top bar items and recipes works fine. Good night dudes
    cya

--- scripts/menuWindowsSwitch.js
+++ scripts/menuWindowsSwitch.js
@@ -12,4 +12,27 @@ $(document).ready(function () {
             $('#' + $this.data('info')).addClass('current');
         })
     });
+
+
+    var $foodBars = $('#food-info-top-bar-categories');
+
+    $foodBars.children().each(function(){
+        var $this = $(this);
+
+        $this.on('click', function () {
+            $('.selected-catagory').removeClass('selected-catagory');
+            $this.addClass('selected-catagory');
+        })
+    });
+
+    var $recipeBars = $('#recipes-top-bar-categories');
+
+    $recipeBars.children().each(function(){
+        var $this = $(this);
+
+        $this.on('click', function () {
+            $('.selected-recipes').removeClass('selected-recipes');
+            $this.addClass('selected-recipes');
+        })
+    });
 });
