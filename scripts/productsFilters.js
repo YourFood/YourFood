b773099115c1b00ff7017881492481fe96d11502
hiksa <skinsomnia@gmail.com>
2014-06-21 05:52:05 -0700

    Product category filter added

--- scripts/productsFilters.js
+++ scripts/productsFilters.js
@@ -12,17 +12,30 @@ $(function () {
         $('#products').empty().append(sorted);
     });
 
+    $('#food-info-top-bar-categories').on('click', 'li', function (ev) {
+        var categoryTitle = ev.target.innerHTML.toLowerCase();
+
+        $.each($('.product-container'), function () {
+            var type = $(this).find('.product-type')[0].innerHTML.toLowerCase();
+
+            if (categoryTitle !== type) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        });
+    });
+
     $('.product-container').on('click', function () {
         var $this = this;
         $this.next('.product-options').slideDown();
     });
 
-
-
     function sortByExpirationDate(first, second) {
-        var remainingDaysFirst = first.find('span').text();
-        var remainingDaysSecond = second.find('span').text();
+        var remainingDaysFirst = $(first).find('span').text();
+        var remainingDaysSecond = $(second).find('span').text();
 
-        return remainingDaysFirst > remainingDaysSecond ? 1 : -1;
+        return parseInt(remainingDaysFirst) > parseInt(remainingDaysSecond) ? 1 : -1;
     }
 });
