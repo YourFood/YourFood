4450acd19b0afad2b6089cee93cede638bf5cb4e
hiksa <skinsomnia@gmail.com>
2014-06-20 22:55:55 -0700

    changes to index

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-(function () {
+var globalManager = (function () {
     var kitchenManager = new kitchenManager.KitchenManager();
     var recipesManager = new recipesManager.RecipesManager();
     var shoppingListManager = new shoppingListManager.ShoppingListManager();
@@ -9,6 +9,12 @@
         kitchenManager.availableProducts = data.currentProducts;
         recipesManager.recipiesList = data.currentRecipes;
         shoppingListManager.currentList = data.shoppingList;
+    }
+
+    return {
+        kitchenManager: kitchenManager,
+        recipesManager: recipesManager,
+        shoppingListManager: shoppingListManager
     }
 }());
 
