9cb2a3a17bc5d7a6369f37568697101ad2cb58da
Yasen <ias_sv_bg@yahoo.co.uk>
2014-06-21 15:08:18 +0300

    added range bar and changed jQuery file name

---
+++ scripts/menuWindowsSwitch.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    var $screenMenu = $('#screen-tabs-menu');
+
+    $screenMenu.children().each(function(){
+        var $this = $(this);
+
+        $this.on('click', function () {
+            $('.selected').removeClass('selected');
+            $this.addClass('selected');
+            $('.current').removeClass('current');
+            $('#' + $this.data('info')).addClass('current');
+        })
+    });
+});
