f42776f91c0b73614068747ccf34b9c483261eee
Yasen <ias_sv_bg@yahoo.co.uk>
2014-06-21 20:01:43 +0300

    finaly jQuery working for recipes

--- scripts/shoppingListSlidedown.js
+++ scripts/shoppingListSlidedown.js
@@ -1,24 +1,23 @@
 $(function () {
-    var $recipeContainer = $('#recipes-content');
-    var $recipe = $('.recipe');
-    console.log($recipe);
-    console.log($recipeContainer);
+    var $recipeContainer = $('#recipies-info-btn');
 
-    $recipe.on('click', function(){
-        console.log('here');
+    $recipeContainer.on('click', function(){
+        var $recipe = $('.recipe');
+
+        $recipe.each(function(){
+            var $this = $(this);
+            $this.on('click', function () {
+                var $description = $this.find('p');
+                console.log($description);
+
+                if ( $description.data('info') === 'hidden-description' ) {
+                    $description.data('info','');
+                    $description.show( "slow" );
+                } else {
+                    $description.data('info', 'hidden-description');
+                    $description.slideUp();
+                }
+            })
+        })
     })
-
-//    $recipeContainer.on('click', function() {
-//        console.log('here');
-//        var $description = $shoppingListContainer.find('p');
-//
-//        if ( $description.data('info') === 'hidden-description' ) {
-//            $description.data('info','');
-//            $description.show( "slow" );
-//        } else {
-//            $description.data('info', 'hidden-description');
-//            $description.find('.shopping-list-products-container').slideUp();
-//        }
-//    })
-
 });
